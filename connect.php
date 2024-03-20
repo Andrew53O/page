89b08fbe6fdc3c0169303179b4edfb6c5c816fc0
ylsyls <149644616+cindycallista@users.noreply.github.com>
2024-03-21 01:10:01 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+$email = $_POST["lemail"];
+$password = $_POST["lpass"];
+$conn = new mysqli('localhost', 'root', 'cindyclstaa1', 'loginpage');
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    $stmt = $conn->prepare("INSERT INTO login (email, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $email, $password);
+    if ($stmt->execute()) {
+        echo "Login data inserted successfully";
+    } else {
+        echo "Error: " . $stmt->error
+    }
+}
+
+?>
